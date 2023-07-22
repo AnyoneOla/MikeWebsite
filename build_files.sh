8d328fc0a76b0ea0b5dabd93b68a1eea534efa5e
AnyoneOla <133249863+AnyoneOla@users.noreply.github.com>
2023-07-22 12:15:06 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -28,6 +28,6 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
 
 echo "Giving permission to files..."
-chmod +rwx name_list.txt
-chmod +rwx song_list.txt
-chmod +rwx songs.txt
+chmod +rwx ./name_list.txt
+chmod +rwx ./song_list.txt
+chmod +rwx ./songs.txt
